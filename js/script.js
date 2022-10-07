7204d4fce82102f1d84776ec669802f78e11c855
Osman Abdullayev <os.abdullayev@gmail.com>
2022-10-08 02:11:42 +0400

    before uploading to github

---
+++ js/script.js
@@ -0,0 +1,142 @@
+// Daily Routine
+console.log("Daily Routine:");
+
+var time = 13;
+
+if (time >= 9 && time < 10) {
+	console.log("Wake Up and Read a book!");
+} else if (time >= 10 && time < 11) {
+	console.log("Have Breakfast");
+} else if (time >= 11 && time < 14) {
+	console.log("Do Homework!");
+} else if (time >= 14 && time < 15) {
+	console.log("Have Lunch!");
+} else if (time >= 15 && time < 17) {
+	console.log("Rest Time!");
+} else if (time >= 17 && time < 19) {
+	console.log("Front-end Courses!");
+} else if (time >= 19 && time < 20) {
+	console.log("Have Dinner!");
+} else if (time >= 20 && time < 21) {
+	console.log("Work Out!");
+} else if (time >= 21 && time < 23) {
+	console.log("Shower and Rest!");
+} else if (time >= 23 && time <= 24) {
+	console.log("Sleep");
+} else if (time >= 0 && time < 9) {
+	console.log("Sleep");
+} else {
+	console.log("Wrong Time!");
+}
+
+// var time = 21;
+
+// switch (true) {
+// 	case time > 24:
+// 		console.log("Time is Invalid...");
+// 		break;
+// 	case time >= 23:
+// 		console.log("Sleep...");
+// 		break;
+// 	case time >= 21:
+// 		console.log("Shower and Rest!!");
+// 		break;
+// 	case time >= 20:
+// 		console.log("Work Out!");
+// 		break;
+// 	case time >= 19:
+// 		console.log("Have Dinner!");
+// 		break;
+// 	case time >= 17:
+// 		console.log("Front End Courses!");
+// 		break;
+// 	case time >= 15:
+// 		console.log("Rest Time");
+// 		break;
+// 	case time >= 14:
+// 		console.log("Have Lunch!");
+// 		break;
+// 	case time >= 11:
+// 		console.log("Do Homework!!");
+// 		break;
+// 	case time >= 10:
+// 		console.log("Have Breakfast");
+// 		break;
+// 	case time >= 9:
+// 		console.log("Wake Up abd Read a Book");
+// 		break;
+// 	case time >= 0:
+// 		console.log("Sleep...");
+// 		break;
+
+// 	default:
+// 		console.log("Enter a valid number.");
+// }
+
+// const object = [
+// 	["a", "b", "c"],
+// 	["m", "n", "o"],
+// 	["x", "y", "z"],
+// ];
+
+// for (const property in object) {
+// 	console.log(`${property}: ${object[property]}`);
+// }
+
+// const object = [
+// 	"Sunday",
+// 	"Monday",
+// 	"Tuesday",
+// 	"Wednesday",
+// 	"Thursday",
+// 	"Friday",
+// 	"Saturday",
+// ];
+
+// for (const property in object) {
+// 	console.log(`${property}: ${object[property]}`);
+// }
+
+// const array1 = [
+// 	["a", "b", "c"],
+// 	["m", "n", "o"],
+// 	["x", "y", "z"],
+// ];
+
+// for (const element of array1) {
+// 	console.log(element);
+// }
+
+// const array1 = [
+// 	"Sunday",
+// 	"Monday",
+// 	"Tuesday",
+// 	"Wednesday",
+// 	"Thursday",
+// 	"Friday",
+// 	"Saturday",
+// ];
+
+// for (const element of array1) {
+// 	console.log(element);
+// }
+
+// let infoArr = [
+// 	["a", "b", "c"],
+// 	["m", "n", "o"],
+// 	["x", "y", "z"],
+// ];
+
+// for (let i = 0; i <= 2; i++) {
+
+// 	for (let k = 0; k <= 2; k++) {
+// 	console.log(infoArr[k][i]);
+
+// 	}
+// }
+
+// let infoArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]    ;
+
+// for (let i = 0; i <= 6; i++) {
+// 	console.log(infoArr[i]);
+// }
